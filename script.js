c667d4e72deb2b4d3fac458af72c025f3767d2df
Daniel Liebert <daniel.liebert@gmail.com>
2022-06-06 15:14:59 -0300

    Add color selector for solid color mode

--- script.js
+++ script.js
@@ -37,71 +37,81 @@ function randomColor() {
 function startCanvas() {
     let pixels = document.querySelectorAll('.pixel');
     pixels.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        if (colorMode === 'randomColor') {
-            element.style.backgroundColor = randomColor();
-        } else if (colorMode === 'black') {
-            element.style.backgroundColor = 'black';
-        } else if (colorMode === 'blackFog') {
-            let color = element.style.backgroundColor;
-            if (color === 'white') {
-                element.style.backgroundColor = 'hsl(0, 0%, 90%)';
-            } else {
-                let rgbArray = color.substr(4);
-                rgbArray = rgbArray.slice(0,rgbArray.length-1);
-                rgbArray = rgbArray.split(', ');
-                let r = rgbArray[0];
-                let g = rgbArray[1];
-                let b = rgbArray[2];
-                if (r !== g || r !== b || g !== b) {
-                    element.style.backgroundColor = 'white';
+        element.addEventListener('mouseover', () => {
+            if (colorMode === 'randomColor') {
+                element.style.backgroundColor = randomColor();
+            } else if (colorMode === 'solid') {
+                element.style.backgroundColor = color;
+            } else if (colorMode === 'blackFog') {
+                let color = element.style.backgroundColor;
+                if (color === 'white') {
+                    element.style.backgroundColor = 'hsl(0, 0%, 90%)';
                 } else {
-                    r = Math.max(r-25,0);
-                    element.style.backgroundColor = `rgb(${r}, ${r}, ${r})`;
+                    let rgbArray = color.substr(4);
+                    rgbArray = rgbArray.slice(0,rgbArray.length-1);
+                    rgbArray = rgbArray.split(', ');
+                    let r = rgbArray[0];
+                    let g = rgbArray[1];
+                    let b = rgbArray[2];
+                    if (r !== g || r !== b || g !== b) {
+                        element.style.backgroundColor = 'white';
+                    } else {
+                        r = Math.max(r-25,0);
+                        element.style.backgroundColor = `rgb(${r}, ${r}, ${r})`;
+                    }
                 }
             }
-        }
-    })
-});
-}
-
-function customizeButtons() {
-
+        })
+    });
 }
 
 let squaresPerSide = 16;
 let width = window.innerWidth*0.7;
 let height = window.innerHeight*0.7;
-let colorMode = 'black';
+let colorMode = 'solid';
+let color = 'black';
+
 createCanvas(squaresPerSide, width, height);
 startCanvas();
 
-resolutionButton = document.querySelector('#resolution');
+let input = document.querySelector('input');
+input.addEventListener('change', () => {
+    color = input.value;
+    colorMode = 'solid';
+    startCanvas();
+    changeColorMode(document.querySelector('#solid'));
+});
+
+let resolutionButton = document.querySelector('#resolution');
 resolutionButton.addEventListener('click', () => {
     squaresPerSide = prompt('Select the number of squares per side');
     cleanCanvas();
     createCanvas(squaresPerSide, width, height);
     startCanvas();
-})
+});
 
 resolutionButton = document.querySelector('#cleanCanvas');
 resolutionButton.addEventListener('click', () => {
     cleanCanvas();
     createCanvas(squaresPerSide, width, height);
     startCanvas();
-})
+});
 
-colorModeButton = document.querySelectorAll('.colorModeButton');
+let colorModeButton = document.querySelectorAll('.colorModeButton');
 colorModeButton.forEach(element => {
     element.addEventListener('click', () => {
-        colorMode = element.id;
-        let activeButtons = document.querySelectorAll('.colorModeButton.active');
-        activeButtons.forEach(element => {
-            element.classList.remove('active');
-        })
-        element.classList.add('active');
+        changeColorMode(element);
+    });
+});
+
+function changeColorMode (element) {
+    colorMode = element.id;
+    let activeButtons = document.querySelectorAll('.colorModeButton.active');
+    activeButtons.forEach(element => {
+        element.classList.remove('active');
     })
-})
+    element.classList.add('active');
+}
 
 window.addEventListener('resize', () => {
     width = window.innerWidth*0.7;
@@ -109,4 +119,4 @@ window.addEventListener('resize', () => {
     cleanCanvas();
     createCanvas(squaresPerSide, width, height);
     startCanvas();
-})
+});
